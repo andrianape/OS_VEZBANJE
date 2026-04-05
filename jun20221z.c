fec1e16db29ae1def982e6d6125c24e2a0184987
student <student>
2026-04-05 22:22:32 +0000

    juncic20221z

---
+++ jun20221z.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+int n;
+
+void* sortiraj(void* arg)
+{
+    int* niz = (int*)arg;
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(niz[i] > niz[j])
+            {
+                int pom = niz[i];
+                niz[i] = niz[j];
+                niz[j] = pom;
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    pthread_t nit;
+    int niz[20];
+    int x;
+    n=0;
+    char* c = (char*)malloc(4*sizeof(char));
+    FILE* f = fopen("ulaz.txt", "r");
+    while(!feof(f))
+    {
+        fscanf(f, "%s", c);
+        x = atoi(c);
+        niz[n++] = x;
+    }
+    pthread_create(&nit, NULL, &sortiraj, niz);
+    pthread_join(nit, NULL);
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", niz[i]);
+    }
+    free(c);
+    return 0;
+}
